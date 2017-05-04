22d8ab6f65cf5ed564844c3b5895f8bc0cbfa2f5
Ickhyun Kwon <ekcode@icloud.com>
2017-05-04 18:34:41 +0900

    plugin for servers

---
+++ plugins/dakao/dakao.plugin.zsh
@@ -0,0 +1,21 @@
+#!/bin/zsh
+
+
+adnet-adfit-admin1     () { ssh -i ~/.ssh/id_rsa_dGate_PC hanadmin@adnet-adfit-admin1.dakao.io }
+adnet-adfit-admin2     () { ssh -i ~/.ssh/id_rsa_dGate_PC hanadmin@adnet-adfit-admin2.dakao.io }
+adnet-adfit-dg1        () { ssh -i ~/.ssh/id_rsa_dGate_PC hanadmin@adnet-adfit-dg1.dakao.io }
+adnet-adfit-front1     () { ssh -i ~/.ssh/id_rsa_dGate_PC hanadmin@adnet-adfit-front1.dakao.io }
+adnet-adfit-front2     () { ssh -i ~/.ssh/id_rsa_dGate_PC hanadmin@adnet-adfit-front2.dakao.io }
+adnet-adfit-mocky      () { ssh -i ~/.ssh/id_rsa_dGate_PC hanadmin@adnet-adfit-mocky.dakao.io }
+adnet-adfit-ora1       () { ssh -i ~/.ssh/id_rsa_dGate_PC hanadmin@adnet-adfit-ora1.dakao.io }
+adnet-adfit-stage1     () { ssh -i ~/.ssh/id_rsa_dGate_PC hanadmin@adnet-adfit-stage1.dakao.io }
+adnet-adfit-stage2     () { ssh -i ~/.ssh/id_rsa_dGate_PC hanadmin@adnet-adfit-stage2.dakao.io }
+adnet-adfit-test1      () { ssh -i ~/.ssh/id_rsa_dGate_PC hanadmin@adnet-adfit-test1.dakao.io }
+adnet-adfit-test2      () { ssh -i ~/.ssh/id_rsa_dGate_PC hanadmin@adnet-adfit-test2.dakao.io }
+adnet-adfit-zeppelin01 () { ssh -i ~/.ssh/id_rsa_dGate_PC hanadmin@adnet-adfit-zeppelin01.dakao.io }
+adnet-batch-app        () { ssh -i ~/.ssh/id_rsa_dGate_PC hanadmin@adnet-batch-app.dakao.io }
+adnet-deploy-app       () { ssh -i ~/.ssh/id_rsa_dGate_PC hanadmin@adnet-deploy-app.dakao.io }
+adnet-external-api1    () { ssh -i ~/.ssh/id_rsa_dGate_PC hanadmin@adnet-external-api1.dakao.io }
+adnet-external-api2    () { ssh -i ~/.ssh/id_rsa_dGate_PC hanadmin@adnet-external-api2.dakao.io }
+adnet-internal-api1    () { ssh -i ~/.ssh/id_rsa_dGate_PC hanadmin@adnet-internal-api1.dakao.io }
+adnet-internal-api2    () { ssh -i ~/.ssh/id_rsa_dGate_PC hanadmin@adnet-internal-api2.dakao.io }
